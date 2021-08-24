48aaee63538cdc871f1517fba60cd656bfb8b333
Daniel Hamacher <danielhamacher.dh@gmail.com>
2021-08-23 23:57:01 -0600

    Updates

--- src/config/config.py
+++ src/config/config.py
@@ -5,7 +5,7 @@ def get_config(environment = 'dev'):
             'log_path': 'C:\\Users\\dhama\\Projects\\image-text-detection-app\\logs',
             'tesseract_config': "--oem 3 --psm 1 -c tessedit_char_whitelist= 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789.$/ '",
             'text_output_path': 'C:\\Users\\dhama\\Projects\\image-text-detection-app\\output',
-            'test_image_path': 'C:\\Users\\dhama\\Projects\\image-text-detection-app\\data\\01_plain_img.png'
+            'test_image_path': 'C:\\Users\\dhama\\Projects\\image-text-detection-app\\data\\alt text.jpg'
         }
     }
     return configuration[environment]
