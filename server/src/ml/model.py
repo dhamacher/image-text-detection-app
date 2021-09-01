96245521ff77463d1e7cd2c3fa6c23e28c905511
Daniel Hamacher <danielhamacher.dh@gmail.com>
2021-09-01 15:14:08 -0600

    fixed import

--- server/src/ml/model.py
+++ server/src/ml/model.py
@@ -11,15 +11,15 @@ import logging
 
 ENV = os.getenv('APP_ENV')
 
-import src.config.config as cfg
+from ..config.config import get_config
 
-_image_path = Path(cfg.get_config(ENV)['image_path'])
+_image_path = Path(get_config(ENV)['image_path'])
 _logger = logging.getLogger(__name__)
 
 
 def _logging_setup():
     try:
-        log_path = Path(cfg.get_config(ENV)['log_path'])
+        log_path = Path(get_config(ENV)['log_path'])
         filename = 'logs.txt'
         dest = log_path / filename
         _logger.setLevel(logging.DEBUG)
@@ -40,7 +40,7 @@ def _logging_setup():
 
 def _get_features(image_array: numpy.ndarray):
     try:
-        # TODO: Study other image feature engineering techniques to imrpove performance.
+        # TODO: Study other image feature engineering techniques to improve performance.
         # featured_image_array1 = bilateral_filter(image_array)
         featured_image_array2 = apply_grayscale(image_array)
         featured_image_array3 = binarization(featured_image_array2)
@@ -55,7 +55,7 @@ def get_text_from_image(image_file: FileStorage):
     log = None
     try:
         log = _logging_setup()
-        tess_exec = Path(cfg.get_config(ENV)['tesseract_cmd'])
+        tess_exec = Path(get_config(ENV)['tesseract_cmd'])
         pytesseract.pytesseract.tesseract_cmd = tess_exec
 
         image_path = save_image(image_file)
@@ -63,9 +63,9 @@ def get_text_from_image(image_file: FileStorage):
         image_array = np.array(image_file)
         processed_image_array = _get_features(image_array)
 
-        custom_config = cfg.get_config('DEV')['tesseract_config']
+        custom_config = get_config(ENV)['tesseract_config']
         output = pytesseract.image_to_string(processed_image_array, config=custom_config)
-        output_path = Path(cfg.get_config('DEV')['text_output_path'])
+        output_path = Path(get_config(ENV)['text_output_path'])
         text_file_path = save_text_to_file(output_path, output)
 
         return output
